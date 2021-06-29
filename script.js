f053e24d09c13cb2e533f9e6782e109bb2c62af2
Miguel Lozano <Miguellozano37578@gmail.com>
2021-06-28 18:48:49 -0700

    second commit

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function getWeatherData(data) {
   var cityName = data.name;
 
   fetch(
-    `http://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&units=imperial&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&units=imperial&appid=${apiKey}`
   )
     .then((res) => res.json())
     .then((info) => {
@@ -60,7 +60,7 @@ function currentWeather(a, b, c) {
   var humidity = b.humidity;
   var wind = b.wind_speed;
   var uv = b.uvi;
-  var icon = `http://openweathermap.org/img/wn/${b.weather[0].icon}.png`;
+  var icon = `https://openweathermap.org/img/wn/${b.weather[0].icon}.png`;
 
   image.setAttribute("src", icon);
   cityName.innerHTML = `${a} (${date})`;
@@ -85,7 +85,7 @@ function fiveDayForecast(daily, timezone) {
 function createCard(fiveday, timezone) {
   // api data
   var unix = fiveday.dt;
-  var wicon = `http://openweathermap.org/img/wn/${fiveday.weather[0].icon}.png`;
+  var wicon = `https://openweathermap.org/img/wn/${fiveday.weather[0].icon}.png`;
   var wtemp = fiveday.temp.day;
   var whumid = fiveday.humidity;
   var wwind = fiveday.wind_speed;
